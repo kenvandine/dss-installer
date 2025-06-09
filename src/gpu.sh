672804928012ea7bf950801cd75e857808d142d4
Ken VanDine <ken.vandine@canonical.com>
2025-06-09 12:35:22 -0400

    Make gpu operator installation quieter

--- src/gpu.sh
+++ src/gpu.sh
@@ -16,14 +16,14 @@ export HOME=$(getent passwd $USER | cut -d: -f6)
 
 curl -fsSL -o /tmp/get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 \
     && chmod 700 /tmp/get_helm.sh \
-    && /tmp/get_helm.sh
+    && /tmp/get_helm.sh 2>/dev/null
 
-helm repo add nvidia https://helm.ngc.nvidia.com/nvidia \
-&& helm repo update
+helm repo add nvidia https://helm.ngc.nvidia.com/nvidia 2>/dev/null \
+&& helm repo update 2>/dev/null
 
-KUBECONFIG=$HOME/.kube/config helm install --wait --generate-name -n gpu-operator --create-namespace nvidia/gpu-operator
+KUBECONFIG=$HOME/.kube/config helm install --wait --generate-name -n gpu-operator --create-namespace nvidia/gpu-operator 2>/dev/null
 
-while ! kubectl logs -n gpu-operator -l app=nvidia-operator-validator | grep "all validations are successful"; do
+while ! kubectl logs -n gpu-operator -l app=nvidia-operator-validator 2>/dev/null | grep "all validations are successful"; do
   echo "Waiting for GPU operator validations to pass..."
   sleep 5
 done
